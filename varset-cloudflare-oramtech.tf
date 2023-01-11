0afa162ea2d861d3b019779bdfb843dd3c1b330a
Ben Oram <b@oram.co>
2023-01-11 01:58:02 +0000

    Added support for Cloudflare varset

--- varset-cloudflare-oramtech.tf
+++ varset-cloudflare-oramtech.tf
@@ -1,4 +1,12 @@
-variable "cloudflare_tunnel_token" {
+variable "cloudflare_account_id" {
+  type = string
+}
+
+variable "cloudflare_api_token" {
+  type = string
+}
+
+variable "cloudflare_dns_zone_id" {
   type = string
 }
 
@@ -8,11 +16,29 @@ resource "tfe_variable_set" "cloudflare" {
   organization  = tfe_organization.oramtech.name
 }
 
-resource "tfe_variable" "cloudflare_tunnel_token" {
-  key = "cloudflare_tunnel_token"
-  description = "Secret token to pass when setting up tunnel"
+resource "tfe_variable" "cloudflare_account_id" {
+  key = "cloudflare_account_id"
+  description = "Account Id for our Cloudflare Zero Trust aka Access domain"
   category = "terraform"
   variable_set_id = tfe_variable_set.cloudflare.id
-  value = var.cloudflare_tunnel_token
+  value = var.cloudflare_account_id
+  sensitive = true
+}
+
+resource "tfe_variable" "cloudflare_api_token" {
+  key = "cloudflare_api_token"
+  description = "Secret token to access the Cloudflare API"
+  category = "terraform"
+  variable_set_id = tfe_variable_set.cloudflare.id
+  value = var.cloudflare_api_token
+  sensitive = true
+}
+
+resource "tfe_variable" "cloudflare_dns_zone_id" {
+  key = "cloudflare_dns_zone_id"
+  description = "Cloudflare Zone Id for DNS"
+  category = "terraform"
+  variable_set_id = tfe_variable_set.cloudflare.id
+  value = var.cloudflare_dns_zone_id
   sensitive = true
 }
